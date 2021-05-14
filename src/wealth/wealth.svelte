7da02e34f50a1a950bba0a17db1ea051c4c5221f
Vincent Slingsby <mironegaines@gmail.com>
2021-05-14 15:35:19 -0700

    Add currency.js for currency functions

--- src/wealth/wealth.svelte
+++ src/wealth/wealth.svelte
@@ -1,56 +1,22 @@
 <script>
 import { wealthGrades } from './wealthgrades.js'
 import WealthBoons from './wealthboons.svelte'
+import { character } from "../stores.js";
+import currencyUtils from "./currency.js"
+import Assets from "./assets.svelte"
 export let pcpRemaining;
 export let selectedWealth;
 export let usedPCP = 0;
 
-
-function calculateCurrency(Gp,Sp,Cp) {
-  let cp = 0;
-  Cp = Cp == "" ? 0 : Cp;
-  Sp = Sp == "" ? 0 : Sp;
-  Gp = Gp == "" ? 0 : Gp;
-  cp += parseInt(Cp);
-  cp += parseInt(Sp) * 12;
-  cp += parseInt(Gp) * 20 * 12;
-  let gp = Math.floor(cp / 240);
-  cp -= (gp * 240);
-  let sp = Math.floor(cp / 12);
-  cp -= (sp * 12);
-  return [gp, sp, cp];
-}
-
-function currencyToString(currency) {
-  let string = '';
-  if (currency[0] > 0) {
-    string += currency[0];
-    string += ' Gp';
-  }
-
-  if (currency[1] > 0) {
-    if (string.length > 0) {
-      string += ', ';
-    }
-    string += currency[1];
-    string += ' Sp';
-  }
-
-  if (currency[2] > 0) {
-    if (string.length > 0) {
-      string += ', ';
-    }
-    string += currency[2];
-    string += ' Cp';
-  }
-  return string;
-}
-
 $: selectedWealthValue = 0;
-$: currentWealth = (currencyToString(selectedWealth.wealth));
+$: currentWealth = (currencyUtils.currencyToString(selectedWealth.wealth));
 $: (selectedWealth = wealthGrades[selectedWealthValue])
 $: (usedPCP = selectedWealth.pcp)
 $: currentWealthPoints = selectedWealth.asset
+
+$: {
+  $character.wealth.socialClass = selectedWealth.name;
+}
 
 </script>
 <h2>Wealth and Social Class</h2>
@@ -64,3 +30,7 @@ $: currentWealthPoints = selectedWealth.asset
 Currency: {currentWealth}, Wealth Points: {currentWealthPoints}
 </p>
 <WealthBoons {selectedWealth} />
+
+{#if selectedWealth.asset > 0}
+<Assets {currentWealthPoints} />
+{/if}
