5846cc6f69db663518be1f800775263e61f58034
Vincent Slingsby <mironegaines@gmail.com>
2021-05-14 16:03:58 -0700

    placeholder armor module

---
+++ src/armor/armor.svelte
@@ -0,0 +1,19 @@
+<script>
+import { armorList } from './armorlist.js'
+import { Button, Col, Row } from "sveltestrap";
+
+</script>
+
+{#each armorList as armor}
+<Row>
+  <Col>
+  <p>{armor.name}</p>
+  </Col>
+  <Col>
+  <p>AV (C/P/B): {armor.avc}/{armor.avp}/{armor.avb}</p>
+  </Col>
+  <Col>
+  <p>{JSON.stringify(armor.coverage)}</p>
+  </Col>
+</Row>
+{/each}
