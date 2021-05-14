7b32b677b42d88e59f0e65089122a26cc92a9c3d
Vincent Slingsby <mironegaines@gmail.com>
2021-05-14 15:34:46 -0700

    New languages

--- src/languages.js
+++ src/languages.js
@@ -12,6 +12,18 @@ export const languages = [
   description: ``
   },
   {
+  name: "Alan",
+  description: ``
+  },
+  {
+  name: "Lendski",
+  description: ``
+  },
+  {
+  name: "Lecian",
+  description: ``
+  },
+  {
   name: "Burdinesi",
   description: ``
   },
@@ -20,8 +32,12 @@ export const languages = [
   description: ``
   },
   {
+  name: "Zinbarik",
+  description: `An archaic ritual language spoken by the Ohanedin. It is a poetic language, distinct from Ohanmihi in that it is impossible to make a promise. It is used by the Thaumaturges of the Ohanedin tribes in order to fulfil their work as the middlemen of the Spirits.`
+  },
+  {
   name: "Zelltongue",
-  description: ``
+  description: `The whistly and chirpy language spoken by all Zells, with cultural variations depending on tribe. The Zelltongue, or Zellish, do have a Lingua Franca, the Yalangi dialect, which they use for diplomacy between tribes.`
   },
   {
   name: "High Ruvian",
@@ -32,7 +48,7 @@ export const languages = [
   description: ``
   },
   {
-  name: "Osterb",
+  name: "Osterbijan",
   description: ``
   },
   {
@@ -40,7 +56,7 @@ export const languages = [
   description: ``
   },
   {
-  name: "Clachish",
+  name: "Clacs",
   description: ``
   },
   {
@@ -53,6 +69,90 @@ export const languages = [
   },
   {
   name: "Flammeshka",
+  description: ``
+  },
+  {
+  name: "Helian",
+  description: ``
+  },
+  {
+  name: "Bluiric",
+  description: ``
+  },
+  {
+  name: "Kyrnic",
+  description: ``
+  },
+  {
+  name: "Praznic",
+  description: ``
+  },
+  {
+  name: "March",
+  description: ``
+  },
+  {
+  name: "Koravi",
+  description: ``
+  },
+  {
+  name: "Kauzer",
+  description: ``
+  },
+  {
+  name: "Ocsi",
+  description: ``
+  },
+  {
+  name: "Lagorian",
+  description: ``
+  },
+  {
+  name: "Karlagorian",
+  description: ``
+  },
+  {
+  name: "Oberan",
+  description: ``
+  },
+  {
+  name: "Martellan",
+  description: ``
+  },
+  {
+  name: "Luzinese",
+  description: ``
+  },
+  {
+  name: "Rus",
+  description: ``
+  },
+  {
+  name: "Svitgardi",
+  description: ``
+  },
+  {
+  name: "Svittish",
+  description: ``
+  },
+  {
+  name: "Berki",
+  description: ``
+  },
+  {
+  name: "Tujanci",
+  description: ``
+  },
+  {
+  name: "Kedouan",
+  description: ``
+  },
+  {
+  name: "Mikanni",
+  description: ``
+  },
+  {
+  name: "Marju",
   description: ``
   },
 ]
